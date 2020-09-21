5f9a0accaf17cdca762f08fcae0c305f13126876
Dennis Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2020-09-21 00:55:46 +0000

    Updated packages

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class HandymanApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.amber,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
@@ -100,6 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        centerTitle: true,
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
