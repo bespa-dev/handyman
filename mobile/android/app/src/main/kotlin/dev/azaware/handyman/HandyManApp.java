147912df28b88fdf4aec8c62b57a8887b9560e48
Quabynah-Codelabs <codelbas.quabynah@gmail.com>
2020-11-17 15:26:59 +0000

    Fixed startup bug

---
+++ mobile/android/app/src/main/kotlin/dev/azaware/handyman/HandyManApp.java
@@ -0,0 +1,23 @@
+package dev.azaware.handyman;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterApplication;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
+import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin;
+
+public class Application extends FlutterApplication implements PluginRegistrantCallback {
+    @override
+    public void onCreate() {
+        super.onCreate();
+        FlutterFirebaseMessagingService.setPluginRegistrant(this);
+    }
+
+    @override
+    public void registerWith(PluginRegistry pluginRegistry) {
+        FirebaseMessagingPlugin.registerWith(pluginRegistry.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
+    }
+}
