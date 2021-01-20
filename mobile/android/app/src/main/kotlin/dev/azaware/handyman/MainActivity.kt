ab5c0256aaab5012d74fda084f173af6a5970bc1
Quabynah-Codelabs <codelbas.quabynah@gmail.com>
2021-01-20 23:28:37 +0000

    removed old app

--- mobile/android/app/src/main/kotlin/dev/azaware/handyman/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package dev.azaware.handyman
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- mobile/lib/domain/models/user.dart
+++
@@ -1,9 +0,0 @@
-/// Base class for all users
-abstract class BaseUser {
-  bool get isCustomer;
-
-  dynamic get user;
-
-  @override
-  String toString() => user.toString();
-}
